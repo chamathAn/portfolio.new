0749398c890807c633f9c8ad5cfc4b7715dc21cb
Chamath Anjula <chamathanjula2@gmail.com>
2026-02-21 23:07:39 +0530

    feat: side panel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "JetBrains Mono",
+        cssVariable: "--font-jetbrains-mono",
+      },
+    ],
+  },
 
   integrations: [react()],
 });
